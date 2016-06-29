08d4279662cc274f0a93e02e190453f1640e4d18
Yu Cang <yhcy1993@gmail.com>
2016-06-29 16:50:14 +0800

    scripts and pics

--- Computational Aerodynamics/script/plotPressureDistribution.py
+++ Computational Aerodynamics/script/plotPressureDistribution.py
@@ -2,32 +2,53 @@ import string
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-    
-data=open('../result/Ma=0.3/alpha=3/pressure_distribution.dat')
 
-up_x=[]
-up_p=[]
+Mach=[0.3,0.8,1.2]
+alpha=[0,3,6,9]
 
-down_x=[]
-down_p=[]
+for Ma in Mach:
+    for angle in alpha:
 
-for line in data:
-    if line=='\n':
-        break
-    (x,y,cp)=line.split()
-    up_x.append(x)
-    up_p.append(cp)
+        up={}
+        down={}
 
-for line in data:
-    (x,y,cp)=line.split()
-    down_x.append(x)
-    down_p.append(cp)
+        input_path="../result/"+str(Ma)+"_"+str(angle)+"_"+"pressure_distribution.dat"
+        output_path="../pic/"+str(Ma)+"_"+str(angle)+"_"+"Cp.png"
 
-plt.scatter(up_x,up_p,label="UP")
-plt.scatter(down_x,down_p,label="DOWN")
-plt.axis([0, 1, 2, -2])
-plt.xlabel("x_pos")
-plt.ylabel("Cp")
-plt.legend()
-plt.show()
+        data=open(input_path)
 
+        for line in data:
+            if line=='\n':
+                break
+            (x,y,cp)=line.split()
+            up[float(x)]=cp
+
+        for line in data:
+            (x,y,cp)=line.split()
+            down[float(x)]=cp
+
+        up=sorted(up.items(),key=lambda d:d[0])
+        down=sorted(down.items(),key=lambda d:d[0])
+
+        up_x=[]
+        up_p=[]
+
+        down_x=[]
+        down_p=[]
+
+        for pos,cp in up:
+            up_x.append(pos)
+            up_p.append(cp)
+
+        for pos,cp in down:
+            down_x.append(pos)
+            down_p.append(cp)
+
+        plt.plot(up_x,up_p,label="UP")
+        plt.plot(down_x,down_p,label="DOWN")
+        plt.xlabel("X")
+        plt.ylabel("Cp")
+        plt.legend()
+        plt.gca().invert_yaxis()
+        plt.savefig(output_path)
+        plt.close()
--- Computational Aerodynamics/script/plotResidue_Coefficients.py
+++ Computational Aerodynamics/script/plotResidue_Coefficients.py
@@ -2,43 +2,53 @@ import string
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+Mach=[0.3,0.8,1.2]
+alpha=[0,3,6,9]
+
+for Ma in Mach:
+    for angle in alpha:
+
+        input_path="../result/"+str(Ma)+"_"+str(angle)+"_"+"convergence_history.dat"
+        err_output_path="../pic/"+str(Ma)+"_"+str(angle)+"_"+"residual.png"
+        coeff_output_path="../pic/"+str(Ma)+"_"+str(angle)+"_"+"cl_cd.png"
+        
+        data=open(input_path)
+
+        index=[]
+        err_rho=[]
+        err_vel=[]
+        err_p=[]
+
+        CL=[]
+        CD=[]
+
+        for record in data:
+            (i,er,ev,ep,cl,cd)=record.split()
+            er=math.log10(float(er))
+            ev=math.log10(float(ev))
+            ep=math.log10(float(ep))
+            index.append(i)
+            err_rho.append(er)
+            err_vel.append(ev)
+            err_p.append(ep)
+            CL.append(cl)
+            CD.append(cd)
     
-data=open('../result/Ma=0.3/alpha=0/convergence_history.dat')
+        plt.plot(index,err_rho,label="err_rho")
+        plt.plot(index,err_vel,label="err_vel")
+        plt.plot(index,err_p,label="err_p")
+        plt.xlabel("Iteration Step")
+        plt.ylabel("log(err)")
+        plt.legend()
+        plt.savefig(err_output_path)
+        plt.close()
 
-index=[]
-err_rho=[]
-err_vel=[]
-err_p=[]
-
-CL=[]
-CD=[]
-
-for record in data:
-    (i,er,ev,ep,cl,cd)=record.split()
-    er=math.log10(float(er))
-    ev=math.log10(float(ev))
-    ep=math.log10(float(ep))
-    index.append(i)
-    err_rho.append(er)
-    err_vel.append(ev)
-    err_p.append(ep)
-    CL.append(cl)
-    CD.append(cd)
-    
-plt.plot(index,err_rho,label="err_rho")
-plt.plot(index,err_vel,label="err_vel")
-plt.plot(index,err_p,label="err_p")
-plt.xlabel("Iteration Step")
-plt.ylabel("log(err)")
-plt.legend()
-plt.savefig('../pic/Residual_0.3_0.png')
-plt.close()
-
-plt.plot(index,CL,label="CL")
-plt.plot(index,CD,label="CD")
-plt.xlabel("Iteration Step")
-plt.ylabel("Aerodynamic coefficients")
-plt.legend()
-plt.savefig('../pic/Coefficient_0.3_0.png')
-plt.close()
+        plt.plot(index,CL,label="CL")
+        plt.plot(index,CD,label="CD")
+        plt.xlabel("Iteration Step")
+        plt.ylabel("Aerodynamic coefficients")
+        plt.legend()
+        plt.savefig(coeff_output_path)
+        plt.close()
 
